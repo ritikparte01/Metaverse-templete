081a4663f45f41f3363335daf4e486b4d472e66b
RITIK PARTE <82569754+ritikparte01@users.noreply.github.com>
2022-03-24 18:43:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+VanillaTilt.init(document.querySelector(".main-img"), {
+    max: 1,
+    speed: 300,
+    easing:"cubic-bezier(.03,.98,.52,.99)", 
+    glare:true,
+    "max-glare":5,     
+    "glare-prerender":true,
+});
+
+VanillaTilt.init(document.querySelector(".main-img2"), {
+    max: 1,
+    speed: 300,
+    easing:"cubic-bezier(.03,.98,.52,.99)", 
+    glare:true,
+    "max-glare":5,     
+    "glare-prerender":true,
+});
+
+VanillaTilt.init(document.querySelector(".main-img3"), {
+    max: 1,
+    speed: 300,
+    easing:"cubic-bezier(.03,.98,.52,.99)", 
+    glare:true,
+    "max-glare":5,     
+    "glare-prerender":true,
+});
